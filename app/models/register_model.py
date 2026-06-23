d9e3252cefac914ed98d8e6fe5fc1695f95a41a9
Shanujan <shanushanujan234@gmail.com>
2026-06-24 01:16:53 +0530

    register model , controller and route created

--- app/models/register_model.py
+++ app/models/register_model.py
@@ -1,12 +1,11 @@
-from extensions import db
-from uuid import uuid4
-from werkzeug.security import generate_password_hash, check_password_hash
-
-class User(db.Model):
-    __tablename__="users"
-    id = db.Column(db.String(), primary_key=True, default =str(uuid4()))
-    username = db.Column(db.String(), nullable=False)
-    email = db.Column(db.String(), nullable=False)
-    password = db.Column(db.Text())
-
-    
+from app.extensions import db
+class Student(db.Model):
+    __tablename__ = "students"
+    student_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password=db.Column(db.Text(),nullable=False)
+    def to_dict(self):
+        return {
+            "student_id": self.student_id,
+            "email": self.email,
+        }
---
+++ app/routes/register_route.py
@@ -0,0 +1,12 @@
+from flask import Blueprint
+
+from app.controllers import register_controller as ctrl
+
+register_bp = Blueprint("students", __name__, url_prefix="/auth/register")
+
+
+@register_bp.route("", methods=["POST"])
+def create_student():
+    return ctrl.create_student()
+
+
